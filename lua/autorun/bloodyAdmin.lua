33c3b5a0740c1c3aa53953b41534b557dec58896
Cruton45 <ClaytonStout1@gmail.com>
2024-07-21 15:44:38 -0400

    Refactor: Changed all includes that used module to be ba_module.

--- lua/autorun/bloodyAdmin.lua
+++ lua/autorun/bloodyAdmin.lua
@@ -3,31 +3,31 @@ bAdmin = {}
 ----------------------------- Module Loading -------------------------------------------
 -- Shared ------------------------------------------------------------------------------
 if SERVER then
-    AddCSLuaFile("modules/sh/commands.lua")
-    AddCSLuaFile("modules/sh/cami.lua")
-    AddCSLuaFile("modules/sh/util.lua")
+    AddCSLuaFile("ba_modules/sh/commands.lua")
+    AddCSLuaFile("ba_modules/sh/cami.lua")
+    AddCSLuaFile("ba_modules/sh/util.lua")
 end
-include("modules/sh/commands.lua")
-include("modules/sh/cami.lua")
-include("modules/sh/util.lua")
+include("ba_modules/sh/commands.lua")
+include("ba_modules/sh/cami.lua")
+include("ba_modules/sh/util.lua")
 -- Server ------------------------------------------------------------------------------
 if SERVER then
-    include("modules/sv/commands/bot.lua")
-    include("modules/sv/ranks.lua")
-    include("modules/sv/player.lua")
-    include("modules/sv/commands/teleport.lua")
-    include("modules/sv/commands/rank.lua")
-    include("modules/sv/commands.lua")
-    include("modules/sv/notification.lua")
+    include("ba_modules/sv/commands/bot.lua")
+    include("ba_modules/sv/ranks.lua")
+    include("ba_modules/sv/player.lua")
+    include("ba_modules/sv/commands/teleport.lua")
+    include("ba_modules/sv/commands/rank.lua")
+    include("ba_modules/sv/commands.lua")
+    include("ba_modules/sv/notification.lua")
 end
 -- Client ------------------------------------------------------------------------------
 if SERVER then
-    AddCSLuaFile("modules/cl/command.lua")
-    AddCSLuaFile("modules/cl/notification.lua")
+    AddCSLuaFile("ba_modules/cl/command.lua")
+    AddCSLuaFile("ba_modules/cl/notification.lua")
 end
 if CLIENT then
-    include("modules/cl/command.lua")
-    include("modules/cl/notification.lua")
+    include("ba_modules/cl/command.lua")
+    include("ba_modules/cl/notification.lua")
 end
 -----------------------------------------------------------------------------------------
 bAdmin_IsLoaded = true
